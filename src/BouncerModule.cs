4d5321cf1b73b09867d6dc3b3620bcf38c2c950f
Kevin Giszewski <kevin@giszewski.com>
2014-02-21 15:22:02 -0500

    Init

---
+++ src/BouncerModule.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.IO;
+using System.Configuration;
+
+namespace Bouncer
+{
+    public class BouncerModule : IHttpModule
+    {
+        private static string offlineFilePath = System.Web.Configuration.WebConfigurationManager.AppSettings["bouncer:offlineFilePath"];
+        private static string ipAcl = System.Web.Configuration.WebConfigurationManager.AppSettings["bouncer:ipAcl"];
+        private static string ignoreBannerPaths = System.Web.Configuration.WebConfigurationManager.AppSettings["bouncer:ignoreBannerPaths"];
+
+        private const string defaultOfflineBanner = @"
+                <script>
+                    function closeBouncerBanner(element){
+                        element.outerHTML = '';
+                        delete element;
+                    }
+                </script>
+                <div onclick='closeBouncerBanner(this)' style='position: fixed; margin-left: 30%; top: 0; width: 30%; color: #000; overflow: hidden; border: 1px solid #000; padding: 10px; background-color: #fff; opacity: 0.9;'>
+                    <span>The website is in offline mode.</span>
+                </div>
+            ";
+        private const string bouncerWatcherAlias = "Bouncer_Watcher";
+        private const string bannerMimeType = "text/html";
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += new EventHandler(Start);
+            context.PreSendRequestContent += new EventHandler(Finish);
+        }
+
+        void Start(object sender, EventArgs e)
+        {
+            var application = (HttpApplication)sender;
+            var context = application.Context;
+
+            //test for file presense which means the site is offline
+            if (File.Exists(context.Server.MapPath(offlineFilePath)))
+            {
+                var userIp = Utility.GetUserIp(application);
+
+                //whitelist check
+                if (!Utility.IsUserIpWhiteListed(userIp, ipAcl))
+                {
+                    //non-admin
+                    context.RewritePath(offlineFilePath);
+                }
+            }
+        }
+
+        void Finish(object sender, EventArgs e)
+        {
+            var application = (HttpApplication)sender;
+            var context = application.Context;
+
+            if (File.Exists(context.Server.MapPath(offlineFilePath)))
+            {
+                var userIp = Utility.GetUserIp(application);
+
+                //whitelist check
+                if (Utility.IsUserIpWhiteListed(userIp, ipAcl))
+                {
+                    var excludedBannerPaths = ignoreBannerPaths.ToLower().Split(',');
+
+                    //test for html/text and add message if not excluded
+                    if (context.Response.ContentType == bannerMimeType && !excludedBannerPaths.Any(x => context.Request.Path.ToLower().StartsWith(x)))
+                    {
+                        var sb = new StringBuilder();
+                        sb.AppendLine(defaultOfflineBanner);
+                        context.Response.Write(sb.ToString());
+                    }
+                }
+            }
+        }
+
+        public void Dispose() { }
+    }
+}
---
+++ src/Utility.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net;
+using LukeSkywalker.IPNetwork;
+
+namespace Bouncer
+{
+    public static class Utility
+    {
+        public static bool IsUserIpWhiteListed(string userIp, string acl)
+        {
+            var aclList = acl.Split(',').ToList();
+
+            //try for a exact IP match
+            if(aclList.Contains(userIp) || userIp == "127.0.0.1"){
+                return true;
+            }
+
+            //try for a network match by CIDR notation; i.e. 10.0.0.0/8
+            var networkList = aclList.Where(x => x.Contains('/'));
+
+            return networkList.Any(x => IPNetwork.Contains(IPNetwork.Parse(x), IPAddress.Parse(userIp)));
+        }
+
+        public static string GetUserIp(HttpApplication application)
+        {
+            var ipList = application.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+            if (!string.IsNullOrEmpty(ipList))
+            {
+                return ipList.Split(',')[0];
+            }
+
+            return application.Request.ServerVariables["REMOTE_ADDR"];
+        }
+    }
+}
